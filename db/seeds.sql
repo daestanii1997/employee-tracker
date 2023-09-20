24e3ea88eb0f4a40ac528b6206e17818a5e5dfc4
Dae <daestanii1997@gmail.com>
2023-09-19 18:59:15 -0600

    created tables and seeds

---
+++ db/seeds.sql
@@ -0,0 +1,14 @@
+INSERT INTO department (name)
+VALUES ("Customer Service"),
+        ("Accounting"),
+        ("Human Resources");
+
+INSERT INTO role (id, title, salary)
+VALUES (101, "Customer Service Rep", 18),
+        (102, "Accountant", 25),
+        (103, "HR Manager", 30); 
+
+INSERT INTO employee (first_name, last_name, manager_id)
+VALUES ("Chester", "Tester", 001),
+        ("Betsy", "Test", 002),
+        ("Test", "Smith", 003);
